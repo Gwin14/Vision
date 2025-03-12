451ab4cfb72c183efefc20edd9d93d6bd8a1abaf
Fábio Santos <fabiospinheira@gmail.com>
2025-03-12 20:37:19 -0300

    drag and drop from sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from 'react';
+import { useCallback, useRef } from "react";
 import {
   ReactFlow,
   Background,
@@ -7,13 +7,18 @@ import {
   addEdge,
   useNodesState,
   useEdgesState,
+  useReactFlow,
+  ReactFlowProvider,
   type OnConnect,
-} from '@xyflow/react';
+} from "@xyflow/react";
 
-import '@xyflow/react/dist/style.css';
+import "@xyflow/react/dist/style.css";
 
-import { initialNodes, nodeTypes } from './nodes';
-import { initialEdges, edgeTypes } from './edges';
+import Sidebar from "./Sidebar";
+import { DnDProvider, useDnD } from "./DnDContext";
+
+import { initialNodes, nodeTypes } from "./nodes";
+import { initialEdges, edgeTypes } from "./edges";
 
 export default function App() {
   const [nodes, , onNodesChange] = useNodesState(initialNodes);
@@ -23,20 +28,80 @@ export default function App() {
     [setEdges]
   );
 
+  let id = 0;
+  const getId = () => `dndnode_${id++}`;
+
+  const DnDFlow = () => {
+    const reactFlowWrapper = useRef(null);
+    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+    const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+    const { screenToFlowPosition } = useReactFlow();
+    const [type] = useDnD();
+
+    const onConnect = useCallback(
+      (params) => setEdges((eds) => addEdge(params, eds)),
+      []
+    );
+
+    const onDragOver = useCallback((event) => {
+      event.preventDefault();
+      event.dataTransfer.dropEffect = "move";
+    }, []);
+
+    const onDrop = useCallback(
+      (event) => {
+        event.preventDefault();
+
+        if (!type) {
+          return;
+        }
+
+        const position = screenToFlowPosition({
+          x: event.clientX,
+          y: event.clientY,
+        });
+        const newNode = {
+          id: getId(),
+          type,
+          position,
+          data: { label: `${type} node` },
+        };
+
+        setNodes((nds) => nds.concat(newNode));
+      },
+      [screenToFlowPosition, type]
+    );
+
+    return (
+      <div className="dndflow">
+        <div className="reactflow-wrapper" ref={reactFlowWrapper}>
+          <ReactFlow
+            nodes={nodes}
+            nodeTypes={nodeTypes}
+            onNodesChange={onNodesChange}
+            edges={edges}
+            edgeTypes={edgeTypes}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            onDrop={onDrop}
+            onDragOver={onDragOver}
+            fitView
+          >
+            <Background />
+            <MiniMap />
+            <Controls />
+          </ReactFlow>
+        </div>
+        <Sidebar />
+      </div>
+    );
+  };
+
   return (
-    <ReactFlow
-      nodes={nodes}
-      nodeTypes={nodeTypes}
-      onNodesChange={onNodesChange}
-      edges={edges}
-      edgeTypes={edgeTypes}
-      onEdgesChange={onEdgesChange}
-      onConnect={onConnect}
-      fitView
-    >
-      <Background />
-      <MiniMap />
-      <Controls />
-    </ReactFlow>
+    <ReactFlowProvider>
+      <DnDProvider>
+        <DnDFlow />
+      </DnDProvider>
+    </ReactFlowProvider>
   );
 }
