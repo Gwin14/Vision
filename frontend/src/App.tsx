6d71dcddeb966b56fe95655a6fc0ac1c05a499bc
Fábio Santos <fabiospinheira@gmail.com>
2025-03-25 19:14:24 -0300

    Onedrive nodes sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { useCallback, useRef } from "react";
+import { useCallback, useRef, useState } from "react";
 import {
   ReactFlow,
   Background,
@@ -11,6 +11,7 @@ import {
   useReactFlow,
   ReactFlowProvider,
   type OnConnect,
+  type Node,
 } from "@xyflow/react";
 
 import "@xyflow/react/dist/style.css";
@@ -21,82 +22,131 @@ import { DnDProvider, useDnD } from "./DnDContext";
 import { initialNodes, nodeTypes } from "./nodes";
 import { initialEdges, edgeTypes } from "./edges";
 
-export default function App() {
-  const [nodes, , onNodesChange] = useNodesState(initialNodes);
+type SelectedNode = Node | null;
+
+function DnDFlow() {
+  const reactFlowWrapper = useRef(null);
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const { screenToFlowPosition } = useReactFlow();
+  const [type] = useDnD();
+  const [selectedNode, setSelectedNode] = useState<SelectedNode>(null);
 
-  let id = 0;
-  const getId = () => `dndnode_${id++}`;
+  const getId = useCallback(() => {
+    return `dndnode_${Date.now()}_${Math.random().toString(36).substr(2, 9)}`;
+  }, []);
 
-  const DnDFlow = () => {
-    const reactFlowWrapper = useRef(null);
-    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-    const [edges, setEdges, onEdgesChange] = useEdgesState([]);
-    const { screenToFlowPosition } = useReactFlow();
-    const [type] = useDnD();
+  const onConnect: OnConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
 
-    const onConnect = useCallback(
-      (params) => setEdges((eds) => addEdge(params, eds)),
-      []
-    );
+  const onDragOver = useCallback((event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "move";
+  }, []);
 
-    const onDragOver = useCallback((event) => {
+  const onDrop = useCallback(
+    (event: React.DragEvent<HTMLDivElement>) => {
       event.preventDefault();
-      event.dataTransfer.dropEffect = "move";
-    }, []);
 
-    const onDrop = useCallback(
-      (event) => {
-        event.preventDefault();
+      if (!type) {
+        return;
+      }
 
-        if (!type) {
-          return;
-        }
+      const position = screenToFlowPosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
 
-        const position = screenToFlowPosition({
-          x: event.clientX,
-          y: event.clientY,
-        });
+      const newNode = {
+        id: getId(),
+        type: type.nodeType,
+        position,
+        data: type.file
+          ? { label: type.file.name, ...type.file }
+          : { label: `${type.nodeType} node` },
+      };
 
-        const newNode = {
-          id: getId(),
-          type: type.nodeType,
-          position,
-          data: type.file
-            ? { label: type.file.name, ...type.file }
-            : { label: `${type.nodeType} node` },
-        };
+      setNodes((nds) => nds.concat(newNode));
+    },
+    [screenToFlowPosition, type, setNodes, getId]
+  );
 
-        setNodes((nds) => nds.concat(newNode));
-      },
-      [screenToFlowPosition, type]
-    );
+  const onNodeClick = useCallback(
+    (event: React.MouseEvent, node: Node) => {
+      event.preventDefault();
+      event.stopPropagation();
+      
+      if (node.type === "file") {
+        setSelectedNode(node);
+      } else {
+        setSelectedNode(null);
+      }
+    },
+    []
+  );
 
-    return (
-      <div className="dndflow">
-        <div className="reactflow-wrapper" ref={reactFlowWrapper}>
-          <ReactFlow
-            nodes={nodes}
-            nodeTypes={nodeTypes}
-            onNodesChange={onNodesChange}
-            edges={edges}
-            edgeTypes={edgeTypes}
-            onEdgesChange={onEdgesChange}
-            onConnect={onConnect}
-            onDrop={onDrop}
-            onDragOver={onDragOver}
-            fitView
-          >
-            <Background />
-            <MiniMap />
-            <Controls />
-          </ReactFlow>
-        </div>
-        <Sidebar />
+  const closeSidebar = useCallback(
+    (event: React.MouseEvent) => {
+      event.preventDefault();
+      event.stopPropagation();
+      setSelectedNode(null);
+    },
+    []
+  );
+
+  return (
+    <div className="dndflow">
+      <div className="reactflow-wrapper" ref={reactFlowWrapper}>
+        <ReactFlow
+          nodes={nodes}
+          nodeTypes={nodeTypes}
+          onNodesChange={onNodesChange}
+          edges={edges}
+          edgeTypes={edgeTypes}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          onDrop={onDrop}
+          onDragOver={onDragOver}
+          onNodeClick={onNodeClick}
+          fitView
+        >
+          <Background />
+          <MiniMap />
+          <Controls />
+        </ReactFlow>
       </div>
-    );
-  };
+      <Sidebar />
+      {selectedNode && (
+        <div 
+          className="floating-sidebar" 
+          onClick={(e) => e.stopPropagation()}
+        >
+          <button
+            type="button"
+            onClick={closeSidebar}
+            className="close-button"
+          >
+            Close
+          </button>
+          <h3>File Metadata</h3>
+          <p>
+            <strong>Name:</strong> {selectedNode.data.name}
+          </p>
+          <p>
+            <strong>ID:</strong> {selectedNode.data.id}
+          </p>
+          <p>
+            <strong>Size:</strong> {selectedNode.data.size}
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
 
+export default function App() {
   return (
     <ReactFlowProvider>
       <DnDProvider>
